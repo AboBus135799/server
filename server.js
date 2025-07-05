48b30b37e4e0db2c3c0befb35c50e58eadecd1dc
AboBus135799 <bebraglot@gmail.com>
2025-07-05 19:43:49 +0300

    changed something idk

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ server.use(cors());
 server.use(bodyParser.json());
 
 server.get('/', (req, res) => {
-    console.log('Кто тебя жестко трахнул со стонами ')
+    console.log('Кто тебя жестко трахнул')
 })
 
 server.listen(PORT, () =>{
